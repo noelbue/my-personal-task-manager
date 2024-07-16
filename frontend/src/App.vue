b79748e7c5b582540b9824d4a540e5ddd00fbfe8
noel.buergler <noel.buergler@topix.ch>
2024-07-17 00:37:44 +0200

    Update App.vue to integrate TaskList component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container mx-auto p-4">
+    <TaskList />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TaskList from './components/TaskList.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    TaskList,
+  },
+};
 </script>
 
 <style>
